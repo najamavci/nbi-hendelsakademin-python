1640aed9155956b8b5faf41f5032ddd8b24548a9
Naja Mukovic <najamukovic@gmail.com>
2026-02-16 00:00:28 +0100

    Veckouppgift 5

---
+++ python05/tdd.py
@@ -0,0 +1,14 @@
+'''
+Samla ihop dina funktioner så att de ligger i en eller flera moduler. Importera och anropa dem från main.py, så att main-filen bara innehåller funktionsanrop.
+
+1a Hitta på lämplig testdata till följande funktion, som omvandlar grader Celsius till grader Fahrenheit.
+def c_to_f(degree):
+    if degree < -273.15:
+        return None
+    return degree * 9 / 5 + 32
+
+1b Vilka ekvivalensklasser har parametern degree?
+
+1c Skriv ett testfall.
+
+'''
